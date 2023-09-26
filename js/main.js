c9746c1469518ff1b7a833fffb7e8c14e1048119
andresdorado13 <andresfdorado.13@gmail.com>
2023-09-26 18:18:32 -0500

    Nuevo modelo service worker modificado

--- js/main.js
+++ js/main.js
@@ -1,46 +1,4 @@
-let swlocation = "sw.js";
-
-if(navigator.serviceWorker){
-  if(window.location.href.includes("localhost")) swlocation="/sw.js";
-  navigator.serviceWorker.register(swlocation);
-}
-
-window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent the mini-infobar from appearing on mobile.
-  event.preventDefault();
-  console.log('👍', 'beforeinstallprompt', event);
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  // Remove the 'hidden' class from the install button container.
-  divInstall.classList.toggle('hidden', false);
-});
-
-butInstall.addEventListener('click', async () => {
-  console.log('👍', 'butInstall-clicked');
-  const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    // The deferred prompt isn't available.
-    return;
-  }
-  // Show the install prompt.
-  promptEvent.prompt();
-  // Log the result
-  const result = await promptEvent.userChoice;
-  console.log('👍', 'userChoice', result);
-  // Reset the deferred prompt variable, since
-  // prompt() can only be called once.
-  window.deferredPrompt = null;
-  // Hide the install button.
-  divInstall.classList.toggle('hidden', true);
-});
-
-window.addEventListener('appinstalled', (event) => {
-  console.log('👍', 'appinstalled', event);
-  // Clear the deferredPrompt so it can be garbage collected
-  window.deferredPrompt = null;
-});
-
-/*if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('./sw.js')
         .then(registration => {
@@ -50,6 +8,4 @@ window.addEventListener('appinstalled', (event) => {
           console.error('Error al registrar el Service Worker:', error);
         });
     });
-}*/
-  
-
+}
