e61dc2068e8ead3521cd10c77c30078fb4c5822e
andresdorado13 <andresfdorado.13@gmail.com>
2023-09-26 16:55:18 -0500

    CSS modificacion

--- sw.js
+++ sw.js
@@ -47,3 +47,13 @@ self.addEventListener('fetch', (event) => {
       })
   );
 });
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the mini-infobar from appearing on mobile.
+  event.preventDefault();
+  console.log('👍', 'beforeinstallprompt', event);
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+  // Remove the 'hidden' class from the install button container.
+  divInstall.classList.toggle('hidden', false);
+});
