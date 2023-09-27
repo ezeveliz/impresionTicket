c6c567ab91731153626c34351c30179e76468988
andresdorado13 <andresfdorado.13@gmail.com>
2023-09-27 09:18:02 -0500

    Arreglo de PWA start url

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const urlsToCache = [
   'images/android-launchericon-512-512.png',
 ];
 
-self.addEventListener("install", (event) => {
+/*self.addEventListener("install", (event) => {
   const cacheStatic = caches
     .open(CACHE_NAME)
     .then((cache) => cache.addAll(urlsToCache));
@@ -38,19 +38,7 @@ self.addEventListener('fetch', (event) => {
     .then(response => response || fetch(event.request))
     .catch(console.log)
   );
-});
-
-/*// sw.js
-
-// Define el nombre de la caché y las URLs que se deben almacenar en la caché
-const CACHE_NAME = 'ticket-printer';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/main.js',
-  '/printer.png',
-];
+});*/
 
 // Instalación del Service Worker
 self.addEventListener('install', (event) => {
@@ -88,4 +76,4 @@ self.addEventListener('fetch', (event) => {
         return response || fetch(event.request);
       })
   );
-});*/
+});
