4c8942d92a477e8edff7d7d548bc0c15ec8ec03c
andresdorado13 <andresfdorado.13@gmail.com>
2023-09-27 08:27:16 -0500

    Arreglo de PWA start url

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js')
-        .then(registration => {
-          console.log('Service Worker registrado con éxito:', registration);
-        })
-        .catch(error => {
-          console.error('Error al registrar el Service Worker:', error);
-        });
-    });
+class Main {
+  constructor() {
+      this.registerServiceWorker();
+  }
+
+  registerServiceWorker() {
+      if ('serviceWorker' in navigator) {
+          window.addEventListener('load', () => {
+              navigator.serviceWorker.register('/sw.js')
+                  .then(registration => {
+                      console.log('Service Worker registrado con éxito:', registration);
+                  })
+                  .catch(error => {
+                      console.error('Error al registrar el Service Worker:', error);
+                  });
+          });
+      }
+  }
 }
+
+window.addEventListener('load', () => {
+  new Main();
+});
