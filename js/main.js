9a662bef6a78b39b955dff9688ca733c55621c48
andresdorado13 <andresfdorado.13@gmail.com>
2023-09-28 12:44:16 -0500

    Preparacion de ambiente

--- js/main.js
+++ js/main.js
@@ -38,58 +38,82 @@ class Main {
     });
   }
 
-  getPdf() {
+}
+
+window.addEventListener('load', () => {
+  new Main();
+});
+
+var selected_device = null;
+
+async function conectarAutomaticamente() {
+  try {
+    // Especifica la dirección MAC u otro identificador único del dispositivo previamente conectado
+    const dispositivoPreviamenteConectado = await navigator.bluetooth.requestDevice({
+      filters: [{ deviceId: { exact: 'DirecciónMAC' } }], // Cambia 'DirecciónMAC' por el identificador real
+      optionalServices: ['battery_service'], // Servicios opcionales a los que te conectarás
+    });
+
+    // Conecta automáticamente al dispositivo previamente conectado
+    await dispositivoPreviamenteConectado.gatt.connect();
+
+    // Asigna el dispositivo previamente conectado a selected_device
+    selected_device = dispositivoPreviamenteConectado;
+    console.log("Conexión exitosa con el dispositivo previamente conectado:", selected_device.name);
+  } catch (error) {
+    console.error("Error al conectar con el dispositivo previamente conectado:", error);
+  }
+}
+
+//Impresora Zebra
+function imprimir() {
+  // Obtener el valor seleccionado en el elemento select
+  var selectedPrinter = document.getElementById("printerSelect").value;
+  // Realizar acciones según la opción seleccionada
+  if (selectedPrinter === "Zebra") {
+    if (!selected_device) {
+      alert("No se ha establecido una conexión con una impresora Zebra.");
+      return;
+    }
+
+    // Realizar la impresión utilizando la conexión Bluetooth previamente establecida
     if (this.fileInput.value === '') {
       console.log('Ningún archivo seleccionado');
       return;
     }
 
     const selectedFile = this.fileInput.files[0];
-
     const reader = new FileReader();
 
     reader.onload = function(event) {
       const binaryString = event.target.result;
       const pdfText = window.btoa(binaryString);
 
-      // Haz lo que necesites con pdfText, por ejemplo, mostrarlo en la consola
-      console.log('Contenido del PDF en base64:', pdfText);
+      // Enviar los comandos ZPL con el contenido del PDF a la impresora Zebra
+      var comandosZPL = "^XA^FO200,200^A0N36,36^FD" + pdfText + "^FS^XZ";
+      selected_device.send(comandosZPL, undefined, errorCallback);
+
+      console.log("Imprimiendo en una impresora Zebra...");
     };
-
     reader.readAsBinaryString(selectedFile);
-  }
-
-  /*function getPdf(callback) {
-    if (document.getElementById("pdf").value === 'none') {
-        pdfText = "";
-    }
-    else {
-      var file = "resource\/" + document.getElementById("pdf").value + "\.pdf";
-
-      var pdfFile = new XMLHttpRequest();
-      pdfFile.callback = callback;
-      pdfFile.open("GET", file, true);
-      pdfFile.responseType = "arraybuffer";
-      pdfFile.onload = function () {
-        var binary = new Uint8Array(this.response);
-        var binaryString = "";
-        for (var i=0; i<binary.byteLength; i++) {
-          binaryString += String.fromCharCode(binary[i]);
-        }
-
-        // base64 encoding
-        pdfText = window.btoa(binaryString);
-
-        this.callback.apply(this, this.argument);
+  } else if (selectedPrinter === "Star") {
+      if (this.fileInput.value === '') {
+        console.log('Ningún archivo seleccionado');
+        return;
       }
-      pdfFile.send(null);
-    }
-
-    createURL();
-  }*/
-
+      const selectedFile = this.fileInput.files[0];
+      const reader = new FileReader();
+      reader.onload = function(event) {
+        const binaryString = event.target.result;
+        const pdfText = window.btoa(binaryString);
+        // Haz lo que necesites con pdfText, por ejemplo, mostrarlo en la consola
+        console.log('Contenido del PDF en base64:', pdfText);
+      };
+      reader.readAsBinaryString(selectedFile);
+      console.log("Imprimiendo en una impresora Star...");
+  } else {
+      alert("Selecciona una impresora válida (Zebra o Star).");
+  }
 }
 
-window.addEventListener('load', () => {
-  new Main();
-});
+conectarAutomaticamente();
