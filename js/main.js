6c3af87737936d7312a67d07ad08de5a1a1666cf
andresdorado13 <andresfdorado.13@gmail.com>
2023-10-06 16:13:26 -0500

    Funcionalidad para el file input

--- js/main.js
+++ js/main.js
@@ -300,9 +300,8 @@ async function pdfToZpl(file) {
     document.body.removeChild(a);
 }
 
-var fileInput = document.getElementById('fileInput');
-
 function inputFileToZpl(){
+  var fileInput = document.getElementById('fileInput');
   var file = fileInput.files[0]; // Obtener el archivo seleccionado
 
   if (file) {
