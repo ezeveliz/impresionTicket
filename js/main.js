e2e68af29390639177048e029e2d04fdf7a23bd1
Lucas Segura <lucas.segura@prcontacto.com.mx>
2023-10-09 12:56:28 -0300

    Changes

--- js/main.js
+++ js/main.js
@@ -110,7 +110,7 @@ function sendFile(fileUrl){
 async function writeToSelectedPrinter()
 { 
   var zpl=await pdfToZpl(fileBackup)
-	selected_device.send(zpl, undefined, errorCallback);
+	selected_device.send("XA^FO200,200^A0N36,36^FDTest Label^FS^XZ", undefined, errorCallback);
 }
 
 /*********************************************** */
