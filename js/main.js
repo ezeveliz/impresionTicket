30db35f494ece8e2026f3a606fb7dc627a32f2fb
andresdorado13 <andresfdorado.13@gmail.com>
2023-10-18 15:44:49 -0500

    Funcionalidad imprimir Zebra y Star con paginas unificadas de PDF

--- js/main.js
+++ js/main.js
@@ -27,12 +27,10 @@ function registerServiceWorker() {
   }
 }
 
-/**************ZEBRA ******************/
 var fileInput;
 var selected_device;
 var devices = [];
 var storedDevices;
-
 let nIntervId;
 var statusTexts = ["Buscando dispositivos", "Buscando dispositivos.", "Buscando dispositivos..", "Buscando dispositivos..."];
 var contenedor = document.getElementById("contenedor");
@@ -91,10 +89,7 @@ function setupZebra(){
   })
 }
 
-async function writeToSelectedPrinter()
-{ 
-  //var zpl=await pdfToZpl(fileBackup)
-	//selected_device.send("^XA^FO50,50^A0N,36,36^FDHello, ZPL^FS^XZ", undefined, errorCallback);
+async function writeToSelectedPrinter(){ 
   var zpl=await pdfToZpl(fileBackup);
   const zplArchive = new Blob([zpl], { type: 'text/plain' });
   selected_device.sendFile(zplArchive, undefined, errorCallback);
@@ -151,7 +146,6 @@ async function pdfToZpl(file) {
   }).reduce((transform, nextTransform) => 
     Math.min(transform, nextTransform)
   );
-  //${425-(initialPosition<125?initialPosition+18:initialPosition)},
   // create content for print.
   let content = '^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR5,5~SD15^JUS^LRN^CI0^XZ^XA^MMT^PW406^LL0480^LS0^XA';
   // loop data for add itens into content;
@@ -216,9 +210,10 @@ function inputFileLoad() {
     var file = fileInput.files[0]; // Obtener el archivo seleccionado
     if (file) {
       displayPdf(file);
-      combineAllPDFPages();
-      //getPdf(createURL);
-      //load();
+      combineAllPDFPages().then(archive => {
+        fileBackup=archive;
+        getPdf(createURL);
+      });
     } else {
       console.error('Ningún archivo seleccionado');
     }
@@ -232,13 +227,13 @@ navigator.serviceWorker.addEventListener("message", (event) => {
   dataTransfer.items.add(file);
   fileInput.files = dataTransfer.files;
   displayPdf(file);
-  getPdf(createURL);
-  //load();
-  //displayFile(file);
+  combineAllPDFPages().then(archive => {
+    fileBackup=archive;
+    getPdf(createURL);
+  });
 });
 
-//FUNCIONES PARA IMPRESORA STAR
-
+/**************************FUNCIONES PARA IMPRESORA STAR*****************************/
 var changeHref;
 
 var pdfText  = "";
@@ -254,331 +249,56 @@ function createURL() {
   document.getElementById("send_data").value = changeHref;
 }
 
-async function combineAllPDFPages() {
-  //const sourcePdfUrl = 'https://example.com/source.pdf'; // URL del PDF fuente
-  //const pdfBytes = await fetch(sourcePdfUrl).then((res) => res.arrayBuffer());
-
-  const pdfDoc = await PDFDocument.create();
-  const sourcePdf = await PDFDocument.load(fileBackup);
-
-  const pagesToCombine = Array.from({ length: sourcePdf.getPageCount() }, (_, i) => i);
-
-  const combinedWidth = Math.max(
-    ...pagesToCombine.map((pageIndex) => sourcePdf.getPages()[pageIndex].getWidth())
-  );
-
-  const combinedHeight = pagesToCombine.reduce(
-    (totalHeight, pageIndex) => totalHeight + sourcePdf.getPages()[pageIndex].getHeight(),
-    0
-  );
-
-  const combinedPage = pdfDoc.addPage([combinedWidth, combinedHeight]);
-
-  let yOffset = combinedHeight;
-  for (const pageIndex of pagesToCombine) {
-    const page = sourcePdf.getPages()[pageIndex];
-    const pageHeight = page.getHeight();
-    yOffset -= pageHeight;
-
-    combinedPage.drawPage(page, {
-      x: 0,
-      y: yOffset,
-      width: page.getWidth(),
-      height: pageHeight,
-    });
-  }
-
-  const combinedPdfBytes = await pdfDoc.save();
-
-  // Crea un enlace para la descarga del PDF
-  const downloadLink = document.getElementById('downloadLink');
-  downloadLink.href = URL.createObjectURL(new Blob([combinedPdfBytes], { type: 'application/pdf' }));
-  
-  // Activa el enlace para que se inicie la descarga
-  downloadLink.style.display = 'block';
-  downloadLink.click();
-}
-
-
-//FUNCIONAL//////////////////////////////////////////
-// function getPdf(callback) {
-//   if (!fileInput.files[0]) {
-//     pdfText = "";
-//   } else {
-//     fileBackup.arrayBuffer().then(resp => {
+function getPdf(callback) {
+  if (!fileInput.files[0]) {
+    pdfText = "";
+  } else {
+    fileBackup.arrayBuffer().then(resp => {
 					
-//       let binary = new Uint8Array(resp);
-//       var binaryString = "";
-//       for (var i=0; i<binary.byteLength; i++) {
-//         binaryString += String.fromCharCode(binary[i]);
-//       }
+      let binary = new Uint8Array(resp);
+      var binaryString = "";
+      for (var i=0; i<binary.byteLength; i++) {
+        binaryString += String.fromCharCode(binary[i]);
+      }
 
-//       // base64 encoding
-//       pdfText = window.btoa(binaryString);
-//       createURL()
-//     })
-//   }
-//   createURL();
-// }
+      // base64 encoding
+      pdfText = window.btoa(binaryString);
+      createURL()
+    })
+  }
+  createURL();
+}
 
 function createPrintToStar(){
-  //pdfMerge();
-  //unifyPdfPages();
   location.href=changeHref;
 }
+/**************************************************************************************/
 
-//////////////////////////////////////////////////////////////////////////////
+async function combineAllPDFPages() {
+  const pdfBytes = await fetch(URL.createObjectURL(fileBackup)).then((res) => res.arrayBuffer());
+  const pdfDoc = await PDFLib.PDFDocument.create();
+  const fileBackupPdf = await PDFLib.PDFDocument.load(pdfBytes);
+  originalPage = await pdfDoc.embedPage(fileBackupPdf.getPages()[0]);
+  preambleDims = originalPage.scale(1.0);
+  const page = pdfDoc.addPage([preambleDims.width,preambleDims.height*fileBackupPdf.getPages().length]);
+  for(paginaActual=0 ; paginaActual<fileBackupPdf.getPages().length ; paginaActual++){
+    originalPage = await pdfDoc.embedPage(fileBackupPdf.getPages()[paginaActual]);
+    preambleDims = originalPage.scale(1.0);
+    page.drawPage(originalPage, {
+      ...preambleDims,
+      x: page.getWidth() / preambleDims.width,
+      y: (page.getHeight() / preambleDims.height)+(preambleDims.height*(fileBackupPdf.getPages().length-(paginaActual+1))),
+    });
+  }
+  const mergedPdfBytes = await pdfDoc.save();
+  const blob = new Blob([mergedPdfBytes], { type: 'application/pdf' });
+  return new File([blob], "file", { type: 'application/pdf' });
+}
 
-// var urls = [
-//   "http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf",
-//   "http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf",
-// ];
-
-// // Disable workers to avoid yet another cross-origin issue (workers need
-// // the URL of the script to be loaded, and dynamically loading a cross-origin
-// // script does not work).
-// //
-// // pdfjsLib.disableWorker = true;
-
-// // In cases when the pdf.worker.js is located at the different folder than the
-// // pdf.js's one, or the pdf.js is executed via eval(), the workerSrc property
-// // shall be specified.
-// //
-// // pdfjsLib.workerSrc = 'pdf.worker.js';
-
-// /**
-//  * @typedef {Object} PageInfo
-//  * @property {number} documentIndex
-//  * @property {number} pageNumber
-//  */
-
-// var pdfDocs = [],
-//   /**
-//    * @property {PageInfo}
-//    */
-//   current = {},
-//   totalPageCount = 0,
-//   pageNum = 1,
-//   pageRendering = false,
-//   pageNumPending = null,
-//   scale = 0.8,
-//   canvas = document.getElementById("the-canvas"),
-//   ctx = canvas.getContext("2d");
-
-// /**
-//  * Get page info from document, resize canvas accordingly, and render page.
-//  * @param num Page number.
-//  */
-// function renderPage(num) {
-//   pageRendering = true;
-//   current = getPageInfo(num);
-//   // Using promise to fetch the page
-//   fileBackup.getPage(current.pageNumber).then(function(page) {
-//     var viewport = page.getViewport({ scale: scale });
-//     canvas.height = viewport.height;
-//     canvas.width = viewport.width;
-
-//     // Render PDF page into canvas context
-//     var renderContext = {
-//       canvasContext: ctx,
-//       viewport: viewport,
-//     };
-//     var renderTask = page.render(renderContext);
-
-//     // Wait for rendering to finish
-//     renderTask.promise.then(function() {
-//       pageRendering = false;
-//       if (pageNumPending !== null) {
-//         // New page rendering is pending
-//         renderPage(pageNumPending);
-//         pageNumPending = null;
-//       }
-//     });
-//   });
-
-//   // Update page counters
-//   document.getElementById("page_num").textContent = pageNum;
-// }
-
-// /**
-//  * If another page rendering in progress, waits until the rendering is
-//  * finished. Otherwise, executes rendering immediately.
-//  */
-// function queueRenderPage(num) {
-//   if (pageRendering) {
-//     pageNumPending = num;
-//   } else {
-//     renderPage(num);
-//   }
-// }
-
-// /**
-//  * Displays previous page.
-//  */
-// function onPrevPage() {
-//   if (pageNum <= 1) {
-//     return;
-//   }
-//   pageNum--;
-//   queueRenderPage(pageNum);
-// }
-
-
-// /**
-//  * Displays next page.
-//  */
-// function onNextPage() {
-//   if (pageNum >= totalPageCount && current.documentIndex + 1 === 1) {
-//     return;
-//   }
-
-//   pageNum++;
-//   queueRenderPage(pageNum);
-// }
-
-// /**
-//  * @returns PageNumber
-//  */
-// function getPageInfo(num) {
-//   let totalPageCount = 0;  
-//   let currentCount = fileBackup;
-//   totalPageCount += currentCount;
-//   if (num <= totalPageCount) {
-//     return {
-//       documentIndex: i,
-//       pageNumber: (currentCount - (totalPageCount - num)),
-//     };
-//   }
-//   return false;
-// }
-
-// function getTotalPageCount() {
-//   var totalPageCount = 0;
-//   for (var docIdx = 0; docIdx < pdfDocs.length; docIdx++) {
-//     totalPageCount += pdfDocs[docIdx].numPages;
-//   }
-//   return totalPageCount;
-// }
-
-// var loadedCount = 0;
-
-// function load() {
-//   // Load PDFs one after another
-//   pdfjsLib.getDocument(URL.createObjectURL(fileBackup)).promise.then(function(pdfDoc_) {
-//     console.log("loaded PDF " + loadedCount);
-//     pdfDocs.push(pdfDoc_);
-//     loadedCount++;
-//     if (loadedCount !== 1) {
-//       return load();
-//     }
-
-//     console.log("Finished loading");
-//     totalPageCount = getTotalPageCount();
-//     document.getElementById("page_count").textContent = totalPageCount;
-
-//     // Initial/first page rendering
-//     renderPage(pageNum);
-//   });
-// }
-
-// async function unifyPdfPages() {
-//   const pdfFile = fileBackup;
-
-//   if (pdfFile) {
-//       const fileReader = new FileReader();
-
-//       fileReader.onload = async function () {
-//           const data = new Uint8Array(this.result);
-
-//           try {
-//               const pdfDoc = await pdfjsLib.getDocument(data).promise;
-//               const canvas = document.getElementById('mergedCanvas');
-//               const context = canvas.getContext('2d');
-              
-//               for (let pageNum = 1; pageNum <= pdfDoc.numPages; pageNum++) {
-//                   const page = await pdfDoc.getPage(pageNum);
-//                   const viewport = await page.getViewport({ scale: 1 });
-
-//                   if (pageNum === 1) {
-//                       canvas.width = viewport.width;
-//                       canvas.height = viewport.height;
-//                   } else {
-//                       canvas.height += viewport.height;
-//                   }
-
-//                   await page.render({
-//                       canvasContext: context,
-//                       viewport: viewport
-//                   }).promise;
-//               }
-
-//               const imgData = canvas.toDataURL('application/pdf', 1.0);
-
-//               // Mostrar el lienzo con la imagen resultante
-//               canvas.style.display = 'block';
-
-//               // Mostrar un enlace para descargar el PDF resultante
-//               const downloadLink = document.getElementById('downloadLink');
-//               downloadLink.href = imgData;
-//               downloadLink.style.display = 'block';
-//           } catch (error) {
-//               console.error('Error al procesar el PDF:', error);
-//           }
-//       };
-
-//       fileReader.readAsArrayBuffer(pdfFile);
-//   } else {
-//       alert('Por favor, seleccione un archivo PDF.');
-//   }
-// }
-
-// function pdfMerge() {
-//   //necessário pois para manter as promisses sincronizadas com await
-//   (async function loop() {
-      
-//           const pdfUrl = URL.createObjectURL(fileBackup);
-//           var loadingTask = await pdfjsLib.getDocument(pdfUrl);
-//           //sem isso fica dessincronizado
-//           await loadingTask.promise.then(function (pdf) {
-//               pdf.getMetadata().then(function (metaData) {
-//                   console.log("pdf (" + urls + ") version: " + metaData.info.PDFFormatVersion); //versão do pdf
-//               }).catch(function (err) {
-//                   console.log('Error getting meta data');
-//                   console.log(err);
-//               });
-//               console.log("páginas: " + pdf.numPages);
-//               let i = 0;
-//               while (i < pdf.numPages) {
-//                   var pageNumber = i;
-//                   pdf.getPage(pageNumber).then(function (page) {
-//                       var div = document.createElement("div");
-//                       var documentosDiv = document.querySelector('#' + 1);
-//                       documentosDiv.appendChild(div);
-//                       var canvas = document.createElement("canvas");
-//                       div.appendChild(canvas);
-//                       // Prepare canvas using PDF page dimensions
-//                       var viewport = page.getViewport({scale: 1, });
-//                       //var canvas = document.getElementById('the-canvas');
-//                       var context = canvas.getContext('2d');
-//                       canvas.height = viewport.height;
-//                       canvas.width = viewport.width;
-//                       // Render PDF page into canvas context
-//                       var renderContext = {
-//                           canvasContext: context,
-//                           viewport: viewport
-//                       };
-//                       var renderTask = page.render(renderContext);
-//                       renderTask.promise.then(function () {
-//                           console.log('Page rendered');
-//                       });
-//                   });
-//                   i++;
-//               }
-//               // Fetch the first page
-//           }, function (reason) {
-//               // PDF loading error
-//               console.error(reason);
-//           });
-      
-//   })();
-// }
+// CODIGO PARA DESCARGAR UN ARCHIVO
+// const url = window.URL.createObjectURL(fileBackup);
+// const a = document.createElement('a');
+// a.href = url;
+// a.download = "fileUnifiedBackup";
+// a.click();
+// window.URL.revokeObjectURL(url);
