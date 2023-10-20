4e0141f1d2422210b396d72f1f3708c1a760e1b7
andresdorado13 <andresfdorado.13@gmail.com>
2023-10-20 14:26:46 -0500

    Arreglo zebra

--- js/main.js
+++ js/main.js
@@ -180,14 +180,14 @@ async function pdfToZpl(file) {
     Math.min(transform, nextTransform)
   );
   // create content for print.
-  let content = '^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR5,5~SD15^JUS^LRN^CI0^XZ^XA^MMT^PW406^LL0480^LS0^XA';
+  let content = '^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR5,5~SD15^JUS^LRN^CI0^XZ^XA^MMT^PW400^LL0480^LS0^XA';
   // loop data for add itens into content;
   //En initial position entre mas grande sea el numero constante, mas alineado a la izquierda estara, en otro caso, mas pequeño a la derecha
   pdf.items.forEach(item => {
     const [fontSize, , , fontWeight, initialPosition, topPosition] = item.transform;
     content += `^FT
                 ${410-initialPosition},
-                ${topPosition - scale + 30}
+                ${topPosition - scale}
                 ^A0I,
                 ${fontSize*(1.4)},
                 ${fontWeight}
