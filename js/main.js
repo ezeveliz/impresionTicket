1f1076c76883522dba2f785fe53dbd078783c9bc
andresdorado13 <andresfdorado.13@gmail.com>
2023-11-09 10:33:04 -0500

    Preparacion de ambiente

--- js/main.js
+++ js/main.js
@@ -595,7 +595,7 @@ function txtRetailSales(textContent){
 }
 
 function txtPurchase(textContent) {
-  let text = '! U1 JOURNAL \r\n! U1 SETLP 7 0 24 \r\n! UTILITIES LT CR-X-LF PRINT \r\n                ';
+  let text = '! U1 JOURNAL \r\n! U1 SETLP 7 0 24 \r\n! UTILITIES LT CR-X-LF PRINT \r\nCOUNTRY LATIN9 \r\n                ';
   let actualContent;
   let afterClient = true;
   let caracteresLineaMax = 0;
